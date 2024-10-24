28830a81a8b16adb2094f877b4387f98c81a7669
sabu-jr <ashilsabu7@gmail.com>
2024-10-24 23:48:29 +0100

    Submitted lines of code

--- Assets/Scripts/Scene2/Scene2DialogueController.cs
+++ Assets/Scripts/Scene2/Scene2DialogueController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+
 public class Scene2DialogueController : MonoBehaviour
 {
     [System.Serializable]
--- Assets/Scripts/Scene2/Scene2Progression.cs
+++ Assets/Scripts/Scene2/Scene2Progression.cs
@@ -5,12 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class Scene2Progression : MonoBehaviour
 {
+    [SerializeField] private Canvas canvas;
+
+    private void Start()
+    {
+        canvas.gameObject.SetActive(false);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-            SceneManager.LoadScene(currentSceneIndex + 1);
+            canvas.gameObject.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            canvas.gameObject.SetActive(false);
         }
     }
 }
